d9c0d879955f42b03fe19752b5ff10883dda1ecd
unknown <abhishekrajpoudel@gmail.com>
2022-12-24 17:47:27 +0545

    Initial setup Done. Header is Done

---
+++ components/Header.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { SocialIcon } from "react-social-icons";
+import { motion } from "framer-motion";
+type Props = {};
+
+export default function Header({}: Props) {
+  return (
+    <header className="sticky  top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
+      <motion.div
+        initial={{ x: -500, opacity: 0, scale: 0.5 }}
+        animate={{ x: 0, opacity: 1, scale: 1 }}
+        transition={{ duration: 1 }}
+        className="flex flex-row item-center"
+      >
+        <SocialIcon
+          url="https://www.linkedin.com/in/abhishek-poudel-56bb74187/"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <SocialIcon
+          url="https://www.linkedin.com/in/abhishek-poudel-56bb74187/"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <SocialIcon
+          url="https://www.linkedin.com/in/abhishek-poudel-56bb74187/"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+      </motion.div>
+      {/* Email */}
+      <motion.div
+        initial={{ x: 500, opacity: 0, scale: 0.5 }}
+        animate={{ x: 0, opacity: 1, scale: 1 }}
+        transition={{ duration: 0.8 }}
+        className="flex flex-row items-center text-gray-300 cursor-pointer"
+      >
+        <SocialIcon network="email" fgColor="gray" bgColor="transparent" />
+        <p className="uppercase hidden md:inline-flex text-sm text-gray-400">
+          Email Here !
+        </p>
+      </motion.div>
+    </header>
+  );
+}
