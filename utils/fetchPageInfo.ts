4b7e76b2a96897734b6e0af44c9b6261fc8533cf
unknown <abhishekrajpoudel@gmail.com>
2023-01-01 19:08:35 +0545

    I was an idiot and replaced localhost with something else my bar

--- utils/fetchPageInfo.ts
+++ utils/fetchPageInfo.ts
@@ -2,18 +2,11 @@
 import { PageInfo } from "../typings";
 
 export const fetchPageInfo = async () => {
-  // Set the Content-Type header to application/json
-  const headers = new Headers();
-  headers.append("Content-Type", "application/json");
-
-  // Make the request with the headers
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/getPageInfo`,
-    { headers: headers }
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/getPageInfo`
   );
 
   const data = await res.json();
-
   // Extract the pageInfo from the data
   const pageInfo: PageInfo = data.pageInfo;
 
