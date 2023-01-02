59d98cf86201129fe9b3d16286478f2e64116d81
unknown <abhishekrajpoudel@gmail.com>
2023-01-02 08:31:07 +0545

    updated version lets see if this will helps

--- sanity/sanity.cli.ts
+++ sanity/sanity.cli.ts
@@ -3,6 +3,6 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'ipqrulj7',
-    dataset: 'production'
-  }
+    dataset: 'production',
+  },
 })
